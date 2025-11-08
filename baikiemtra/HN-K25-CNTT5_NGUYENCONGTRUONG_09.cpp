ded58b8bb966be3dafa9940794bd159ae11c7939
xuanbinh622006-alt <xuanbinh622006@gmail.com>
2025-11-08 09:27:51 +0700

    HN-K25-CNTT5_NGUYENCONGTRUONG_09.cpp

--- baikiemtra/HN-K25-CNTT5_NGUYENCONGTRUONG_09.cpp
+++ baikiemtra/HN-K25-CNTT5_NGUYENCONGTRUONG_09.cpp
@@ -7,7 +7,10 @@
 		int vi_tri ;//vi tri muon them trong mang
 		int gia_tri;// gia tri muon them trong mang
 		int xoa;
-		
+		int a, b;
+		 int temp;
+    	int x;
+    	int ton_tai = 0;
 		
 		do{
 		printf("-------------------------MENU---------------------------- \n");
@@ -67,38 +70,87 @@
 				break;
 			
 			case 5:
-				printf("nhap vi tri muon them:");
-				scanf("%d",& vi_tri);
-				
-				printf("nhap gia tri muon them:");
-				scanf("%d" ,& gia_tri);
-				
-				for(int i=0 ; i> vi_tri; i--){
-					phan_tu [vi_tri]= phan_tu [vi_tri-1];
-					i++;
-				}
-				phan_tu[vi_tri]=gia_tri;
-					
-				printf("phan mang sau khi them la");
-				for(int i=0; i>n; i++){
-					printf("%d",phan_tu[i]);
-				}
-				printf("\n");
-				break ;
-			
-			case 6:
-				printf("nhap vi tri muon xoa :");
-				scanf("%d",&xoa);
-					
-				for(int i=0 ; i> xoa; i++){
-					phan_tu [xoa]= phan_tu [xoa+1];
-					i--;
-					}
-				for(int i=0; i>n; i++){
-					printf("%d",phan_tu[i]);
-					}
-				printf("\n");
-				break ;
+                printf("Nhap vi tri muon them (0 -> %d): ", n);
+                scanf("%d", &vi_tri);
+                if (vi_tri < 0 || vi_tri > n) {
+                    printf("Vi tri khong hop le!\n");
+                    break;
+                }
+                printf("Nhap gia tri muon them: ");
+                scanf("%d", &gia_tri);
+                for (int i = n; i > vi_tri; i--) {
+                    phan_tu[i] = phan_tu[i - 1];
+                }
+                phan_tu[vi_tri] = gia_tri;
+                n++;
+                printf("Mang sau khi them: ");
+                for (int i = 0; i < n; i++) {
+                    printf("%d ", phan_tu[i]);
+                }
+                printf("\n");
+                break;
+
+            case 6:
+                printf("Nhap vi tri muon xoa (0 -> %d): ", n - 1);
+                scanf("%d", &xoa);
+                if (xoa < 0 || xoa >= n) {
+                    printf("Vi tri khong hop le!\n");
+                    break;
+                }
+                for (int i = xoa; i < n - 1; i++) {
+                    phan_tu[i] = phan_tu[i + 1];
+                }
+                n--;
+                printf("Mang sau khi xoa: ");
+                for (int i = 0; i < n; i++) {
+                    printf("%d ", phan_tu[i]);
+                }
+                printf("\n");
+                break;
+			 case 7:
+                for (int i = 0; i < n - 1; i++) {
+                    for (int j = i + 1; j < n; j++) {
+                        if (phan_tu[i] < phan_tu[j]) {
+                            temp = phan_tu[i];
+                            phan_tu[i] = phan_tu[j];
+                            phan_tu[j] = temp;
+                        }
+                    }
+                }
+                printf("Mang sau khi sap xep giam dan: ");
+                for (int i = 0; i < n; i++) {
+                    printf("%d ", phan_tu[i]);
+                }
+                printf("\n");
+                break;
+
+            case 8:
+                printf("Nhap gia tri can kiem tra: ");
+                scanf("%d", &x);
+                ton_tai = 0;
+                for (int i = 0; i < n; i++) {
+                    if (phan_tu[i] == x) {
+                        ton_tai = 1;
+                        break;
+                    }
+                }
+                if (ton_tai)
+                    printf("Gia tri %d co ton tai trong mang!\n", x);
+                else
+                    printf("Gia tri %d khong ton tai trong mang!\n", x);
+                break;
+
+            case 9:
+                printf("Nhap khoang [a, b]:\n");
+                printf("a = "); scanf("%d", &a);
+                printf("b = "); scanf("%d", &b);
+                printf("Cac phan tu trong khoang [%d, %d]: ", a, b);
+                for (int i = 0; i < n; i++) {
+                    if (phan_tu[i] >= a && phan_tu[i] <= b)
+                        printf("%d ", phan_tu[i]);
+                }
+                printf("\n");
+                break;
 				
 					
 				
@@ -113,3 +165,4 @@
 
 		return 0;
 	}
+
